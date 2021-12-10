f5244c8949bb7f58fcc3a9b1ac0b23590c495b58
Adam Azira <aladdintirta@gmail.com>
2021-12-10 17:26:55 +0700

    Adding profiles

--- src/components/cards/details.js
+++ src/components/cards/details.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Details = () => {
+const Details = ({id}) => {
 
 	// const [] = useState("")
 	
 	return (
 		<div>
 			<h1>THis is the detail post</h1> 
+			<p>details: {{id}}</p>
 		</div>
 	)
 }
